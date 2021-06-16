fde5826ed1b4252305daedbe5b72b49e5beaf39d
Joe Plt <empous@mail.com>
2021-06-15 22:20:52 -0400

    edited files

--- flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -23,6 +23,6 @@ def forth(id):
 
 
 if __name__=="__main__" :
-    app.run('host=0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
     #app.run(debug=True)--developing env
 
--- flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
+++ flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -15,11 +15,8 @@ def sum():
 
 
 
-
-
-
 if __name__=="__main__":
-    app.run('host=0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
     
     
     #app.run(debug=True)
