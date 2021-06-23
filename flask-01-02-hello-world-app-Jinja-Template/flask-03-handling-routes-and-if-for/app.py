31c6ccd4a4a026c3b3827cf9284100a1281ff281
Joe Plt <empous@mail.com>
2021-06-22 21:41:11 -0400

    my applications

---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-03-handling-routes-and-if-for/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask (__name__)
+
+
+@app.route('/')
+def head():
+
+    first="this is my first condition experience"
+    return render_template("index.html", message=False)
+
+@app.route('/empo')
+def header():
+    name = {"sedar","empo","ali","fatma"}
+    return render_template("body.html",object = name)
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
